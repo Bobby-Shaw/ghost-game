fade6ce394232e654dc8ceb16ef508e201e37b10
bobbyshaw <shawuk2006@gmail.com>
2025-12-31 11:48:39 +0000

    Added function for checking highscores

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalTime;
+import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name;
-        int score = 0, roundNumber = 1, doorBound = 2, ghostDoor, doorGuess;
+        
+        int score = 0, roundNumber = 1, doorBound = 6, ghostDoor, doorGuess;
         boolean alive = true;
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.print("Enter name: ");
-        name = scanner.nextLine();
-
         System.out.println("""
-                
                 Welcome to ghost game!
                 Guess a door and avoid the spooky ghost to stay alive
                 """);
@@ -26,7 +20,7 @@ public class Main {
             System.out.println("ROUND " + roundNumber);
             ghostDoor = random.nextInt(1, doorBound);
 
-            System.out.print("Guess a door between 1 and "+ (doorBound-1) +": ");
+            System.out.print("Guess a door between 1 and " + (doorBound - 1) + ": ");
             doorGuess = scanner.nextInt();
 
             // checks if user inputs a correct door number
@@ -41,31 +35,54 @@ public class Main {
                 score += 1;
                 roundNumber += 1;
 
-            // checks if player is scared from the ghost
+                // checks if player is scared from the ghost
             } else {
                 scanner.close();
                 System.out.println("\nBoo!");
                 System.out.println("Unlucky");
-                System.out.println("Your final Score: "+score);
+                System.out.println("Your final Score: " + score + "\n");
                 alive = false;
             }
         }
-        saveGame(name, score);
+        checkHighscore(score);
     }
 
     // Saves name and score in a text file
-    public static void saveGame(String name, int score) {
-        LocalTime localTime = LocalTime.now();
-        String fileLocation = "src/save.txt";
-        String saveString = name + ", " + score + ", " + localTime + "\n";
+    private static void saveGame(int score) {
+        String filePath = "src/save.txt";
+        String saveString = "" + score;
 
         // writes name and score into file
-        try (FileWriter fileWriter = new FileWriter(fileLocation)) {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
             fileWriter.write(saveString);
 
         } catch (IOException e) {
             System.out.println("Unable to save game!");
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void checkHighscore(int score) {
+        String filePath = "./src/save.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+
+            try {
+                int currentHighscore = Integer.parseInt(line);
+                if (score > currentHighscore) {
+                    System.out.println("New Highscore!");
+                    saveGame(score);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Couldn't convert string to integer!");
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file!");
+        } catch (IOException e) {
+            System.out.println("Something went wrong!");
         }
     }
 }
